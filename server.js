abb8b562fac2b29820a8cb86fc572c4bb11f9dbb
visaso <32449867+visaso@users.noreply.github.com>
2020-05-08 11:20:12 +0300

    server.js cleanup

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const mongoose = require('mongoose');
+const http = require('http');
+
+const https = require('https');
+const fs = require('fs');
 
 const db = require('./utils/db')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const passport = require('./utils/pass.js')
+const authRoute = require('./routes/authRoute')
 
 const graphQlHttp = require('express-graphql');
 const schema = require('./schema/schema');
 
 app.use(cors());
-
 db.on('connected', () => {
     process.env.NODE_ENV = process.env.NODE_ENV || "development";
     if (process.env.NODE_ENV === "production") {
@@ -32,7 +42,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/post', require('./routes/postRoute'));
 
-//app.use('/auth', require('./routes/authRoute'));
+app.use('/auth', require('./routes/authRoute'));
 
 app.use('/graphql', (req, res) => {
     graphQlHttp({schema, graphiql: true, context: {req, res}})(req,
