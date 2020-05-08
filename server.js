1edf03feff5537295c33aea2410e27a12729132b
visaso <32449867+visaso@users.noreply.github.com>
2020-05-08 11:18:13 +0300

    server.js cleanup

--- server.js
+++ server.js
@@ -3,35 +3,15 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const path = require('path');
-
-const mongoose = require('mongoose');
-const http = require('http');
-
-const https = require('https');
-const fs = require('fs');
 
 const db = require('./utils/db')
 const cors = require('cors')
-const bodyParser = require('body-parser')
-
-const passport = require('./utils/pass.js')
-const authRoute = require('./routes/authRoute')
-
-//const sslkey = fs.readFileSync('./ssl-key.pem');
-//const sslcert = fs.readFileSync('./ssl-cert.pem')
 
 const graphQlHttp = require('express-graphql');
 const schema = require('./schema/schema');
 
-
 app.use(cors());
-/*
-const options = {
-      key: sslkey,
-      cert: sslcert
-};
-*/
+
 db.on('connected', () => {
     process.env.NODE_ENV = process.env.NODE_ENV || "development";
     if (process.env.NODE_ENV === "production") {
@@ -45,7 +25,6 @@ db.on('connected', () => {
     }
  });
 
-
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
@@ -53,13 +32,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/post', require('./routes/postRoute'));
 
-app.use('/auth', require('./routes/authRoute'));
-
-/*
-app.get('/', (req, res) => {
-    res.send('Hello Secure World!');
-});
-*/
+//app.use('/auth', require('./routes/authRoute'));
 
 app.use('/graphql', (req, res) => {
     graphQlHttp({schema, graphiql: true, context: {req, res}})(req,
